0395c36924f633b350d889169a4194818afed42e
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2018-08-10 07:20:24 -0400

    refactored event resources, changed to functional component instead of class component

--- src/components/Event/EventList.js
+++ src/components/Event/EventList.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import EventListItem from "./EventListItem";
 
-class EventList extends Component{
-	render() {
-		const { events, onEventOpen, deleteEvent } = this.props;
-		
-		const eventItem = events.map(event =>{
-			return <EventListItem key={event.id} event={event} onEventOpen={onEventOpen} deleteEvent={deleteEvent} />
-		});
+const EventList = (props) =>{
+	const { events, onEventOpen, deleteEvent } = props;
+	
+	const eventItem = events.map(event =>{
+		return <EventListItem key={event.id} event={event} onEventOpen={onEventOpen} deleteEvent={deleteEvent} />
+	});
 
-		return (
-			<div>
-				{ eventItem }
-			</div>
-		);
-	}
+	return (
+		<div>
+			{ eventItem }
+		</div>
+	);
 }
 
 export default EventList;
--- src/components/Event/EventListItem.js
+++ src/components/Event/EventListItem.js
@@ -1,53 +1,51 @@
 import React, { Component } from 'react';
 import EventPatrons from "./EventPatronsList";
 
-class EventListItem extends Component {
-	render() {
-		const { event, onEventOpen, deleteEvent } = this.props;
+const EventListItem = (props) =>{
+	const { event, onEventOpen, deleteEvent } = props;
+	
+	return (
+		<div className="panel panel-default">
+			<div className="panel-heading">
+				<div className="row">
+					<div className="col-sm-2">
+						<a href="#">
+							<img src={event.hostPhotoURL} alt="Avatar" className="img-circle thumbnailImg"/>
+						</a>
+					</div>
+					<div className="col-sm-10">
+						<h3 className="panel-title">{event.title}</h3>
+						<p><b>Hosted By:</b> {event.hostedBy}</p><hr/>
 
-		return (
-			<div className="panel panel-default">
-				<div className="panel-heading">
-					<div className="row">
-						<div className="col-sm-2">
-							<a href="#">
-								<img src={event.hostPhotoURL} alt="Avatar" className="img-circle thumbnailImg"/>
-							</a>
-						</div>
-						<div className="col-sm-10">
-							<h3 className="panel-title">{event.title}</h3>
-							<p><b>Hosted By:</b> {event.hostedBy}</p><hr/>
-
-							<div className="panel-meta">
-								<p>
-									<span className="event-time">
-										<i className="fas fa-clock"></i>Today, {event.date}
-									</span>
-									<span className="event-location">
-									 	<i className="fas fa-map"></i> {event.city}
-									</span> 
-									<span><a onClick={deleteEvent(event.id)}><i className="fas fa-trash"></i></a></span>
-								</p>
-							</div>
+						<div className="panel-meta">
+							<p>
+								<span className="event-time">
+									<i className="fas fa-clock"></i>Today, {event.date}
+								</span>
+								<span className="event-location">
+								 	<i className="fas fa-map"></i> {event.city}
+								</span> 
+								<span><a onClick={deleteEvent(event.id)}><i className="fas fa-trash"></i></a></span>
+							</p>
 						</div>
 					</div>
 				</div>
-
-				<div className="panel-body">
-					<ul className="list-inline">
-						{event.attendees && event.attendees.map((guest) =>{
-							return <EventPatrons key={guest.id} guest={guest} />
-						})}
-					</ul>
-				</div>
-
-				<div className="panel-footer clearfix">
-					<span>{event.description}</span>
-					<span className="pull-right"><a onClick={onEventOpen(event)} className="btn btn-primary btn-sm">VIEW</a></span>
-				</div>
 			</div>
-		);
-	}
+
+			<div className="panel-body">
+				<ul className="list-inline">
+					{event.attendees && event.attendees.map((guest) =>{
+						return <EventPatrons key={guest.id} guest={guest} />
+					})}
+				</ul>
+			</div>
+
+			<div className="panel-footer clearfix">
+				<span>{event.description}</span>
+				<span className="pull-right"><a onClick={onEventOpen(event)} className="btn btn-primary btn-sm">VIEW</a></span>
+			</div>
+		</div>
+	);
 }
 
 export default EventListItem;
