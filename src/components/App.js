8c720c46be2447736b0142d487c6d918f8e237da
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2018-08-10 17:14:06 -0400

    landing page added

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import NavBar from "./Layout/Navigation/Navbar";
 import EventDashboard from "./Event/EventsDashboard";
 import EventForm from "./Event/EventForm/EventForm";
@@ -13,20 +13,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 class App extends Component {
   render() {
     return (
-    	<div>
-    		<NavBar />
-	      <div className="container">
-          <main> 
-           <Route exact path="/" component={Home} />
-           <Route path="/events" component={EventDashboard} />
-           <Route path="/events/new" component={EventForm} />
-           <Route path="/events/:id" component={EventDetails} />
-           <Route path="/users" component={UsersDashboard} />
-           <Route path="/users/:id" component={UserProfile} />
-           <Route path="/settings" component={SettingsDashboard} />
-          </main>
-	      </div>
-	    </div>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+        <Route path="/(.+)" render={() =>(
+          <div>
+            <NavBar />
+            <div className="container">
+              <main>
+                <Switch>
+                  <Route path="/events" component={EventDashboard} />
+                  <Route path="/events/new" component={EventForm} />
+                  <Route path="/events/:id" component={EventDetails} />
+                  <Route path="/users" component={UsersDashboard} />
+                  <Route path="/users/:id" component={UserProfile} />
+                  <Route path="/settings" component={SettingsDashboard} />
+                </Switch>
+              </main>
+            </div>
+          </div>
+        )} />
+      </div>
     );
   }
 }
--- src/components/Home/Homepage.js
+++ src/components/Home/Homepage.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import "./Home.css";
 
 const Home = (props) => {
+	const { history } = props;
+
   return (
-  	<h1>Home Page</h1>    
+  	<header id="header" className="header">
+			<div className="header-overlay">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-12">
+							<div className="header-text">
+								<h1>Welcome to 247Events</h1>
+								<p><span onClick={() => history.push("/events")} className="btn btn-danger">Get Started</span></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>   
   );
 };
 
