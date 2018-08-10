e55fa9c85b855744fa780b9390bd7125474199fd
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2018-08-10 13:26:32 -0400

    event detail component created

---
+++ src/components/Event/EventDetails.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react';
+
+
+const EventDetails = (props) => {
+  return (
+		<div>
+			<h1>Event Details Page</h1>
+		</div>      
+  );
+};
+
+
+export default EventDetails;
